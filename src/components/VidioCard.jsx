e7e6f2bff7ea36675a186ca026cf143eeac45862
Rohit mishra <mishrarohit27112005@gmail.com>
2025-02-02 01:41:22 -0800

    correct mitake

--- src/components/VidioCard.jsx
+++ src/components/VidioCard.jsx
@@ -7,10 +7,10 @@ const VidioCard = ({ info }) => {
 
 
   return (
-    <div className="md:w-80 w-full flex flex-col md:h-96 py-8">
+    <div className="md:w-80 w-full flex flex-col overflow-hidden md:h-96 py-8">
       <img className="rounded" src={thumbnails?.medium?.url} loading="lazy" alt="" />
       <ul>
-        <li className="py-3 font-bold w-96">{title}</li>
+        <li className="py-3 font-bold ">{title}</li>
         <li className="pb-2 font-medium">{channelTitle}</li>
         <li>{viewCount}Views</li>
       </ul>
